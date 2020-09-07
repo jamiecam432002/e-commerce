d82443c2c62a96e558823342765d8aed50cfe373
Jamie Cameron <jamiecam43@gmail.com>
2020-09-07 13:03:07 -0400

    finishing signin handlesubmit method

--- src/components/form-input/form-input.js
+++ src/components/form-input/form-input.js
@@ -3,7 +3,7 @@ import "./form-input.styles.scss";
 
 const FormInput = ({ handleChange, label, ...props }) => (
   <div className="group">
-    <input className="form-input" onClick={handleChange} {...props} />
+    <input className="form-input" onChange={handleChange} {...props} />
     {label ? (
       <label
         className={`${props.value.length ? "shrink" : ""} form-input-label`}
