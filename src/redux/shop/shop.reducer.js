af505c1201338e222063430b0177a824db22efae
Jamie Cameron <jamiecam43@gmail.com>
2020-09-13 17:27:44 -0400

    fixed issue with loading collections on collection pages

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,15 +1,29 @@
-import * as actionTypes from "./shop.types";
+import ShopActionTypes from "./shop.types";
 
 const initialState = {
   collections: null,
+  isFetching: false,
+  errorMessage: "",
 };
 
 const shopReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.UPDATE_COLLECTIONS:
+    case ShopActionTypes.FETCH_COLLECTIONS_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
       return {
         ...state,
         collections: action.payload,
+        isFetching: false,
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload,
       };
     default:
       return state;
